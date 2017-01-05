ad450c7b9caba3ef57da400b572e54428d3f91e8
Philipp Otto <philipp@scm.io>
2017-01-05 17:10:11 +0100

    display warning if diff3 style could not be detected

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+  var diff3StyleDetected;
+  var beginMarker = "<<<<<<< HEAD";
+  var baseMarker = "||||||| merged common ancestors";
+  var versionBMarker = "=======";
+  var endMarker = ">>>>>>>";
+
   function diff3ToVersions(diff3) {
     // 0: common
     // 1: version A
@@ -10,15 +16,15 @@ $(document).ready(function() {
       b : [],
       base : []
     };
-
     diff3.split("\n").forEach(function(line) {
-      if (line.indexOf("<<<<<<< HEAD") === 0) {
+      if (line.indexOf(beginMarker) === 0) {
         state = 1;
-      } else if (line.indexOf("||||||| merged common ancestors") === 0) {
+      } else if (line.indexOf(baseMarker) === 0) {
         state = 2;
-      } else if (line.indexOf("=======") === 0) {
+        diff3StyleDetected = true;
+      } else if (line.indexOf(versionBMarker) === 0) {
         state = 3;
-      } else if (line.indexOf(">>>>>>>") === 0) {
+      } else if (line.indexOf(endMarker) === 0) {
         state = 0;
       } else {
         if (state === 0) {
@@ -73,8 +79,14 @@ $(document).ready(function() {
   }
 
   $("#diff-button").click(function() {
+    diff3StyleDetected = false;
     var diff3 = document.getElementById("conflict-input").value;
     var versions = diff3ToVersions(diff3);
     generateAndDrawDiffs(versions);
+    if (diff3StyleDetected) {
+      $("#warning").addClass("hidden");
+    } else {
+      $("#warning").removeClass("hidden");
+    }
   });
 });
